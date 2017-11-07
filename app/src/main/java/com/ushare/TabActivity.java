83a309ca164db4a69bcbc8ff4b334ef0ea5af1da
David <david_thedragonzzz@yahoo.com>
2017-11-07 17:37:33 +0700

    tampilkan icon filter pada toolbar

--- app/src/main/java/com/ushare/TabActivity.java
+++ app/src/main/java/com/ushare/TabActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -78,6 +79,13 @@ public class TabActivity extends AppCompatActivity {
 
         tabs = (TabLayout)findViewById(R.id.tabs);
         tabs.setupWithViewPager(pager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_order, menu);
+        return true;
     }
 
     @Override
